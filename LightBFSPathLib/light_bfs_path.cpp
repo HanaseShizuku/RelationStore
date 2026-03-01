d5ba5bf66ac7759f1a65c4f9c75d9272e151cdf8
Shizuku <visualproducerofficial@outlook.com>
2026-03-01 21:39:21 +0800

    读取图的方法

--- LightBFSPathLib/light_bfs_path.cpp
+++ LightBFSPathLib/light_bfs_path.cpp
@@ -6,78 +6,80 @@
 #include <map>
 #include <sstream>
 #include <stdexcept>
-
-#define PUBLIC public:
-#define PRIVATE private:
+#include <span>
+#include "file.hpp"
+#include "string.hpp"
 
 namespace LightBFSPathLib
 {
     using namespace std;
+    using namespace shizuku::io;
+    using namespace shizuku::util::string;
     namespace fs = std::filesystem;
     using Graph = map<string, set<string>>;
     using Path = fs::path;
 
     class LightBFSPath
     {
-        PRIVATE Graph _graph;
-        PRIVATE Path _tablePath;
-        PRIVATE ifstream _tablereader;
-        PUBLIC LightBFSPath(Path tablePath) : _tablePath(move(tablePath))
+    private:
+        Graph _graph;
+        Path _tablePath;
+        ifstream _tablereader;
+        bool _isInstance = false;
+
+    public:
+        LightBFSPath(Path tablePath) : _tablePath(move(tablePath))
         {
-            if (!fs::exists(_tablePath))
+            if (!File::Exists(_tablePath))
             {
-                throw fs::filesystem_error(
-                    "Cannot open patch table",
-                    _tablePath,
-                    std::make_error_code(std::errc::no_such_file_or_directory));
-                _tablereader=ifstream(_tablePath);
+                throw std::runtime_error("File not exist: " + _tablePath.string());
+                return;
             }
+            _isInstance = true;
         }
-        PUBLIC bool ReadGraph()
+        bool ReadGraph()
         {
+            vector<string> v = File::ReadAllLines(_tablePath);
+            for (const auto &s : v)
+            {
+                auto lineFull = Split(s, ' ');
+                if (lineFull.size() < 3)
+                {
+                    throw std::runtime_error("Each line shall contain at least 2 elements excluding the connection mode identifier: " + s);
+                    return false;
+                }
+                auto lineElement = span(lineFull).subspan(1);
+                if (lineFull[0] == "+")
+                {
+                    // 行内元素全连接
+                    for (size_t i = 0; i < lineElement.size(); ++i)
+                    {
+                        for (size_t j = i + 1; j < lineElement.size(); ++j)
+                        {
+                            const auto &a = lineElement[i];
+                            const auto &b = lineElement[j];
+                            _graph[a].insert(b);
+                            _graph[b].insert(a);
+                        }
+                    }
+                }
+                else if (lineFull[0] == "-")
+                {
+                    // 行内除连接方式标识符以外的所有元素与第一个元素单向连接 方向:第一个元素->别的元素
+                    std::string beginPos = lineElement[0];
+                    for (const auto &endPos : span(lineElement).subspan(1))
+                    {
+                        _graph[beginPos].insert(endPos);
+                    }
+                }
+                else
+                {
+                    throw std::runtime_error("Syntax error in connecting graph elements: " + s);
+                    return false;
+                }
+            }
+            return true;
         }
     };
 
-    void PatchGraph(Graph &graph, fs::path patchTablePath)
-    {
-        if (!fs::exists(patchTablePath))
-        {
-            return;
-        }
-        std::string line;
-        ifstream patchTable(patchTablePath);
-        if (!patchTable.is_open())
-        {
-            return;
-        }
-        while (getline(patchTable, line))
-        {
-            stringstream ss(line);
-            string word;
-            vector<string> table;
-            while (ss >> word)
-            {
-                table.push_back(word);
-            }
-            if (table.size() < 2)
-            {
-                continue;
-            }
-            for (auto &a : table)
-            {
-                for (auto &b : table)
-                {
-                    if (a != b)
-                    {
-                        graph[a].insert(b);
-                        graph[b].insert(a);
-                    }
-                }
-            }
-        }
-    }
-
 }
-
-#undef PRIVATE
-#undef PUBLIC
